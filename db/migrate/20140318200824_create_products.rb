e11c003b7ea7997e87ee49a694a8a03af9abdcff
Helen Hood <helen.c.hood@gmail.com>
2014-03-19 17:55:58 -0400

    Add find_or_create_by in migration

--- db/migrate/20140318200824_create_products.rb
+++ db/migrate/20140318200824_create_products.rb
@@ -8,12 +8,13 @@ class CreateProducts < ActiveRecord::Migration
 
     Sale.find_each do |sale|
       # create a new Product object
-      unless Product.exists?(name: sale.product_name)
-        Product.create(name: sale.product_name)
-        puts "Product #{sale.product_name} created!"
-      else
-        puts "Product #{sale.product_name} already existed!"
-      end
+      # unless Product.exists?(name: sale.product_name)
+      #   Product.create(name: sale.product_name)
+      #   puts "Product #{sale.product_name} created!"
+      # else
+      #   puts "Product #{sale.product_name} already existed!"
+      # end
+      Product.find_or_create_by(name: sale.product_name)
     end
   end
 end
